ca86351f89800a7dbd763160bd75a8b61c3268cb
Suryansh Narang <suryanshnarang23@gmail.com>
2024-09-09 20:44:10 +0530

    isStudent middleware update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -18,9 +18,9 @@ exports.auth = async (req, res) => {
     }
     //verify token now on what basis??? secret key
     try {
-      const decode = await jwt.verify(token, process.env.JWT_SECFRET);
-      console.log(decode);
-      req.user = decode;
+      const decode = jwt.verify(token, process.env.JWT_SECFRET);
+      console.log(decode); //user role will also be displayed becuase of auth controller
+      req.user = decode; //and user role stored in request
     } catch (error) {
       //verification isssue
       res.staus(401).json({
@@ -31,11 +31,30 @@ exports.auth = async (req, res) => {
     next();
   } catch (error) {
     return res.status(401).json({
-        success: false,
-        message: "Something wen wrong while validating token",
-    })
+      success: false,
+      message: "Something wen wrong while validating token",
+    });
   }
 };
 //isStudent
+exports.isStudent = async (req, res, next) => {
+  //at the time of login controller we created a payload which contained user role
+  try {
+    //1) req m se we can find out first method
+    //2) second can be taking from DB accountType
+    //but we will go with first method as we already have the data
+    if (req.user.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "User is not a student",
+      });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Something wen wrong while validating user role",
+    });
+  }
+};
 //isInstructor
 //isAdmin
