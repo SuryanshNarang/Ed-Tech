5145c9495c6294d2248f369030a344d401f5cd0f
Suryansh Narang <suryanshnarang23@gmail.com>
2024-09-11 22:11:32 +0530

    Course js workflow done

--- controllers/Course.js
+++ controllers/Course.js
@@ -3,3 +3,17 @@ const Tag = require("../models/Tag");
 const User = require("../models/User");
 //course thumbnail is there so we will have to use cloudinary
 
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+//create course handler function 
+exports.createCourse=async(req,res)=>{
+    try{
+        
+
+
+
+    }catch(error){
+
+    }
+}
+
+//get all course handler function
