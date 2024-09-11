a0ffa9fe6a2d979d423f86f7bd20333c4ee762ee
Suryansh Narang <suryanshnarang23@gmail.com>
2024-09-11 21:40:57 +0530

    add imageUploader in util

---
+++ controllers/Course.js
@@ -0,0 +1,5 @@
+const Course = require("../models/Course");
+const Tag = require("../models/Tag");
+const User = require("../models/User");
+//course thumbnail is there so we will have to use cloudinary
+
